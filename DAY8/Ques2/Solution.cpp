d88dd2bd8337a0df91d59bd1a4a91adb18ba701a
Sudhir Maurya <98456936+Sudhir769@users.noreply.github.com>
2023-12-07 00:17:54 +0530

    DAY8

--- DAY8/Ques2/Solution.cpp
+++ DAY8/Ques2/Solution.cpp
@@ -3,12 +3,52 @@ using namespace std;
 
 class Solution {
 public:
-    
+    int equalPairs(vector<vector<int>>& grid) {
+        int n = grid.size();
+
+        map<vector<int>, int> freq;
+        int count = 0;
+
+        for(auto i:grid){
+            freq[i]++;
+        }
+
+        for(int i=0;i<n;i++){
+            vector<int>temp;
+            for(int j=0;j<n;j++){
+                temp.push_back(grid[j][i]);
+            }
+            count += freq[temp];
+        }
+        return count;
+    }
 };
 
 int main(){
     int t; cin>>t;
     while(t--){
-        
+        int n; cin>>n;
+        vector<vector<int>>grid(n, vector<int>(n,0));
+
+        for(int i=0;i<n;i++){
+            for(int j=0;j<n;j++){
+                cin>>grid[i][j];
+            }
+        }
+        Solution ob;
+
+        int ans = ob.equalPairs(grid);\
+        cout<<ans<<endl;
     }
 }
+//Input
+// 2
+// 3
+// 3 2 1
+// 1 7 6
+// 2 7 7
+// 4
+// 3 1 2 2
+// 1 4 4 5
+// 2 4 2 2
+// 2 4 2 2 
--- DAY8/Ques3/Solution.cpp
+++ DAY8/Ques3/Solution.cpp
@@ -3,12 +3,42 @@ using namespace std;
 
 class Solution {
 public:
-    
+    string removeStars(string s) {
+        int n = s.size();
+        stack<char>st;
+        string ans = "";
+
+        for(int i=0;i<n;i++){
+            if(s[i] != '*'){
+                st.push(s[i]);
+            }else{
+                if(!st.empty())st.pop();
+            }
+        }
+        while(!st.empty()){
+            ans += st.top(); 
+            st.pop();
+        }
+        reverse(ans.begin(), ans.end());
+        return ans;
+    }
 };
 
 int main(){
     int t; cin>>t;
     while(t--){
+        string s;
+        cin>>s;
+
+        Solution ob;
+        string ans = ob.removeStars(s);
         
+        for(auto i:ans){
+            cout<<i;
+        }cout<<endl;
     }
 }
+// Input
+// 2
+// leet**cod*e
+// abcdef*****
