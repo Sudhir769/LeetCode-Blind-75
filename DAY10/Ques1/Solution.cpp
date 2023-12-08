40998eca783cdec6808fbd4c5c328a4768499d2f
Sudhir Maurya <98456936+Sudhir769@users.noreply.github.com>
2023-12-08 23:33:08 +0530

    DAY10

--- DAY10/Ques1/Solution.cpp
+++ DAY10/Ques1/Solution.cpp
@@ -1,9 +1,39 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+// Definition for singly-linked list.
+struct ListNode {
+    int val;
+    ListNode *next;
+    ListNode() : val(0), next(nullptr) {}
+    ListNode(int x) : val(x), next(nullptr) {}
+    ListNode(int x, ListNode *next) : val(x), next(next) {}
+};
+
 class Solution {
 public:
-    
+    ListNode* oddEvenList(ListNode* head) {
+        
+        ListNode odd(0);
+        ListNode even(0);
+
+        ListNode *t1 = &odd, *t2 = &even;
+        int index=1;
+        while(head){
+            if(index%2==1){
+                t1->next = head;
+                t1=t1->next;
+            }else{
+                t2->next=head;
+                t2=t2->next;
+            }
+            index++;
+            head=head->next;
+        }
+        t1->next = even.next;
+        t2->next=NULL;
+        return odd.next;
+    }
 };
 
 int main(){
--- DAY10/Ques3/Solution.cpp
+++ DAY10/Ques3/Solution.cpp
@@ -1,9 +1,35 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+struct ListNode {
+    int val;
+    ListNode *next;
+    ListNode() : val(0), next(nullptr) {}
+    ListNode(int x) : val(x), next(nullptr) {}
+    ListNode(int x, ListNode *next) : val(x), next(next) {}
+};
+
 class Solution {
 public:
-    
+    string predictPartyVictory(string senate) {
+        int n = senate.size();
+        queue<int> rad, dir;
+        for(int i=0;i<n;i++){
+            if(senate[i]=='R') rad.push(i);
+            else dir.push(i);
+        }
+        while(!rad.empty() and !dir.empty()){
+            if(rad.front() > dir.front()) dir.push(n++);
+            else rad.push(n++);
+
+            rad.pop();
+            dir.pop();
+        }
+        if(rad.empty()){
+            return "Dire";
+        }
+        return "Radiant";
+    }
 };
 
 int main(){
