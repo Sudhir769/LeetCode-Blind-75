e476ea6b2e846a2f85126df6050696392d4935ee
Sudhir Maurya <98456936+Sudhir769@users.noreply.github.com>
2023-12-15 20:48:34 +0530

    DAY15

--- DAY15/Ques3/Solution.cpp
+++ DAY15/Ques3/Solution.cpp
@@ -1,11 +1,49 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-
-
 class Solution {
 public:
-    
+    int ans = 0;
+    void bfs(int src, unordered_map<int, list<pair<int, bool>>> &adjList){
+        queue<int> q;
+        unordered_map<int, bool> visited;
+        q.push(src);
+
+        visited[src] = true;
+
+        while(!q.empty()){
+            int front = q.front();
+            q.pop();
+
+            for(auto neighbour : adjList[front]){
+                int v = neighbour.first;
+                bool check = neighbour.second;
+
+                if(!visited[v]){
+                    if(check == 1){
+                        ans++;
+                    }
+                    visited[v] = true;
+                    q.push(v);
+                }
+            }
+        }
+    }
+    int minReorder(int n, vector<vector<int>>& connections) {
+        unordered_map<int, list<pair<int, bool>>> adjList;
+
+        for(int i = 0; i < connections.size(); i++){
+            int u = connections[i][0];
+            int v = connections[i][1];
+
+            adjList[u].push_back({v, 1}); //original edge
+            adjList[v].push_back({u, 0}); //fake edge
+        } 
+
+        bfs(0, adjList);
+
+        return ans;
+    }
 };
 
 int main(){
