7571824679678da220ad99e10602bdbbac028fed
Sudhir Maurya <98456936+Sudhir769@users.noreply.github.com>
2023-12-17 22:45:11 +0530

    DAY16

--- DAY16/Ques2/Solution.cpp
+++ DAY16/Ques2/Solution.cpp
@@ -2,10 +2,51 @@
 using namespace std;
 
 
-
 class Solution {
 public:
-    
+    bool isAtBorder(int row, int col, int n, int m) {
+        return row == 0 || row == n - 1 || col == 0 || col == m - 1;
+    }
+    int nearestExit(vector<vector<char>>& maze, vector<int>& entrance) {
+        int n = maze.size();
+        int m = maze[0].size();
+        
+        queue<pair<pair<int, int>, int>>q; //{{row, col}, steps}
+
+        int rowStart = entrance[0];
+        int colStart = entrance[1];
+        q.push({{rowStart, colStart}, 0});
+
+        int delRow[] = {-1, 0, 1, 0};
+        int delCol[] = {0, 1, 0, -1};
+
+        maze[rowStart][colStart] = '+';
+        int cntSteps = 0;
+        while(!q.empty()){
+            int row = q.front().first.first;
+            int col = q.front().first.second;
+            int steps = q.front().second;
+
+            q.pop();
+
+            if(isAtBorder(row, col, n, m)==1 and make_pair(row, col) != make_pair(rowStart, colStart)){
+                return steps;
+            }
+
+            for(int ind=0; ind<4; ind++){
+                int nrow = row + delRow[ind];
+                int ncol = col + delCol[ind];
+
+                if(nrow>=0 and nrow<n and ncol>=0 and ncol<m){
+                    if(maze[nrow][ncol] != '+'){
+                        q.push({{nrow, ncol}, steps+1});
+                        maze[nrow][ncol] = '+';
+                    }
+                }
+            }
+        }
+        return -1;
+    }
 };
 
 int main(){
--- DAY16/Ques3/Solution.cpp
+++ DAY16/Ques3/Solution.cpp
@@ -5,7 +5,53 @@ using namespace std;
 
 class Solution {
 public:
-    
+    int orangesRotting(vector<vector<int>>& grid) {
+        int n = grid.size();
+        int m = grid[0].size();
+
+        int vis[n][m];
+        int cntfresh=0;
+        queue<pair<pair<int, int>, int>>q;
+        for(int i=0;i<n;i++){
+            for(int j=0;j<m;j++){
+                if(grid[i][j]==2){
+                    q.push({{i, j}, 0});
+                    vis[i][j]=2;
+                }else{
+                    vis[i][j] = 0;
+                }
+                if(grid[i][j]==1) cntfresh++;
+            }
+        }
+        int maxTime = 0;
+        int dx[4] = {-1, 0, 1, 0};
+        int dy[4] = {0, 1, 0, -1};
+        int cnt=0;
+        while(!q.empty()){
+            auto it = q.front();
+            q.pop();
+            int row = it.first.first;
+            int col = it.first.second;
+            int t = it.second;
+
+            maxTime = max(maxTime, t);
+
+            for(int ind = 0; ind < 4; ind++){
+                int nrow = row + dx[ind];
+                int ncol = col + dy[ind];
+
+                if(nrow>=0 and nrow<n and ncol>=0 and ncol<m){
+                    if(vis[nrow][ncol] !=2 and grid[nrow][ncol]==1){
+                        q.push({{nrow, ncol}, t+1});
+                        vis[nrow][ncol] = 2;
+                        cnt++;
+                    }
+                }
+            }
+        }
+        if(cnt!=cntfresh) return -1;
+        return maxTime;
+    }
 };
 
 int main(){
