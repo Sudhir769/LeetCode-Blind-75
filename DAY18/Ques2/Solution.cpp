d124c0d2ea6181f1a842bb9a00f589e149435766
Sudhir Maurya <98456936+Sudhir769@users.noreply.github.com>
2023-12-21 23:20:49 +0530

    DAY18

--- DAY18/Ques2/Solution.cpp
+++ DAY18/Ques2/Solution.cpp
@@ -16,17 +16,17 @@ public:
         long long int mid = n/2;
         int e=n, s=1;
         
-        while(s<e){
-            mid = s + (e-s)/2;
+        // while(s<e){
+        //     mid = s + (e-s)/2;
 
-            if(guess(mid)==0){ //guess is defined internally on leetcode
-                return mid;
-            }else if(guess(mid)==-1){
-                e=mid-1;
-            }else if(guess(mid)==1){
-                s=mid+1;
-            }
-        }
+        //     if(guess(mid)==0){ //guess is defined internally on leetcode
+        //         return mid;
+        //     }else if(guess(mid)==-1){
+        //         e=mid-1;
+        //     }else if(guess(mid)==1){
+        //         s=mid+1;
+        //     }
+        // }
         return s;
     }
 };
