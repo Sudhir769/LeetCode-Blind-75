8af29ea8df2987b5a20234e94df46479891b386e
Sudhir Maurya <98456936+Sudhir769@users.noreply.github.com>
2023-12-21 23:19:38 +0530

    DAY18

--- DAY18/Ques2/Solution.cpp
+++ DAY18/Ques2/Solution.cpp
@@ -1,11 +1,34 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-
+/** 
+ * Forward declaration of guess API.
+ * @param  num   your guess
+ * @return 	     -1 if num is higher than the picked number
+ *			      1 if num is lower than the picked number
+ *               otherwise return 0
+ * int guess(int num);
+ */
 
 class Solution {
 public:
-    
+    int guessNumber(int n) {
+        long long int mid = n/2;
+        int e=n, s=1;
+        
+        while(s<e){
+            mid = s + (e-s)/2;
+
+            if(guess(mid)==0){ //guess is defined internally on leetcode
+                return mid;
+            }else if(guess(mid)==-1){
+                e=mid-1;
+            }else if(guess(mid)==1){
+                s=mid+1;
+            }
+        }
+        return s;
+    }
 };
 
 int main(){
--- DAY18/Ques3/Solution.cpp
+++ DAY18/Ques3/Solution.cpp
@@ -5,7 +5,34 @@ using namespace std;
 
 class Solution {
 public:
-    
+    vector<int> successfulPairs(vector<int>& spells, vector<int>& potions, long long success) {
+        int n = spells.size();
+        int m = potions.size();
+
+        sort(potions.begin(), potions.end());
+
+        vector<int>pairs(n);
+        int j = 0;
+        for(long long i: spells){
+            long long start = 0, end = m-1, mid = start + (end - start)/2;
+            int ans=-1;
+            while(start <= end){
+                mid = start + (end - start)/2;
+
+                if(i*potions[mid] >= success){
+                    ans = mid;
+                    end = mid-1;
+                }else{
+                    start = mid+1;
+                }
+            }
+
+            pairs[j++] = ans != -1 ? (m - ans):0;
+        }
+
+        
+        return pairs;
+    }
 };
 
 int main(){
