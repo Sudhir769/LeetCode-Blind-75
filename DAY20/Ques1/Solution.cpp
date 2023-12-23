b0396ac168de68ad94bd583aded58189adb04bc4
Sudhir Maurya <98456936+Sudhir769@users.noreply.github.com>
2023-12-24 01:26:28 +0530

    DAY20

--- DAY20/Ques1/Solution.cpp
+++ DAY20/Ques1/Solution.cpp
@@ -4,7 +4,27 @@ using namespace std;
 
 class Solution {
 public:
-    
+    vector<vector<int>>ans;
+    void solve(int ind, int k, int sum, vector<int>&sumVector, int n){
+        if(sum==n and k==0){
+            ans.push_back(sumVector);
+            return;
+        }
+        if(sum > n) return;
+
+        for(int i=ind; i<=9; i++){
+            if(i>n) break;
+            sumVector.push_back(i);
+            solve(i+1, k-1, sum+i, sumVector, n);
+            sumVector.pop_back();
+        }
+    }
+    vector<vector<int>> combinationSum3(int k, int n) {
+        int sum=0;
+        vector<int>sumVector;
+        solve(1, k, sum, sumVector, n);
+        return ans;    
+    }
 };
 
 int main(){
--- DAY20/Ques3/Solution.cpp
+++ DAY20/Ques3/Solution.cpp
@@ -5,7 +5,22 @@ using namespace std;
 
 class Solution {
 public:
-    
+    vector<int> dp;
+    int solve(int n, vector<int> &cost){
+        if(n<=1) return 0;
+        
+        if(dp[n] != -1) return dp[n];
+        
+        int one = solve(n-1, cost) + cost[n-1];
+        int two = solve(n-2, cost) + cost[n-2];
+
+        return dp[n] =  min(one, two);
+    }
+    int minCostClimbingStairs(vector<int>& cost) {
+        int n = cost.size();
+        dp.resize(n+1, -1);
+        return solve(n, cost);
+    }
 };
 
 int main(){
