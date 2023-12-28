5b4aa0ef74c669e092d8f11b9c13ea64c31b4aa3
Sudhir Maurya <98456936+Sudhir769@users.noreply.github.com>
2023-12-28 23:26:30 +0530

    DAY21

--- DAY22/Ques2/Solution.cpp
+++ DAY22/Ques2/Solution.cpp
@@ -5,7 +5,25 @@ using namespace std;
 
 class Solution {
 public:
-    
+    vector<vector<int>>dp;
+    int solve(int n, vector<int>& prices, int fee, int buy){
+        if(n == prices.size()) return 0;
+        if(dp[n][buy] != -1)  return dp[n][buy];
+
+        int profit = 0;
+        if(buy){
+            profit = max(-prices[n] - fee + solve(n+1, prices, fee, 0), solve(n+1, prices, fee, 1));
+        }else{
+            profit = max(prices[n] + solve(n+1, prices, fee, 1), solve(n+1, prices, fee, 0));
+        }
+        return dp[n][buy] = profit;
+    }
+
+    int maxProfit(vector<int>& prices, int fee) {
+        int n = prices.size();
+        dp.resize(n+1, vector<int>(2, -1));
+        return solve(0, prices, fee, 1);
+    }
 };
 
 int main(){
--- DAY22/Ques3/Solution.cpp
+++ DAY22/Ques3/Solution.cpp
@@ -5,7 +5,25 @@ using namespace std;
 
 class Solution {
 public:
-    
+    vector<vector<int>>dp;
+    int solve(int i, int j, string& s1, string& s2){
+        if(i<0) return j+1;
+        if(j<0) return i+1;
+
+        if(dp[i][j] != -1) return dp[i][j];
+        if(s1[i]==s2[j]){
+            return dp[i][j] = solve(i-1, j-1, s1, s2);
+        }
+        return dp[i][j] = min({1+solve(i, j-1, s1, s2), 1+solve(i-1, j, s1, s2), 1+ solve(i-1, j-1, s1, s2)});
+        return dp[i][j] = false;
+    }
+
+    int minDistance(string word1, string word2) {
+        int n = word1.size();
+        int m = word2.size();
+        dp.resize(n+1, vector<int>(m+1, -1));
+        return solve(n-1, m-1, word1, word2);
+    }
 };
 
 int main(){
